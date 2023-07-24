791476303b09765b790de1185e90e4ccd4c44f18
Phạm Quang Anh <106795680+Anhpq2001@users.noreply.github.com>
2023-07-24 10:01:47 +0700

    add PrintWriterDemo

---
+++ src/PrintWriterDemo/Main.java
@@ -0,0 +1,11 @@
+package PrintWriterDemo;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File file = new File("src/PrintWriterDemo/test1.txt");
+
+    }
+}
